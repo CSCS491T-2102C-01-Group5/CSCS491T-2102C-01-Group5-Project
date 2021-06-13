f91c63a88558c8654be2d2fd69e1fe9fca16ba5d
CSCS491T-2102C-01-Group5 <84294824+CSCS491T-2102C-01-Group5@users.noreply.github.com>
2021-06-12 17:52:01 -0700

    replaced database.sql and dummy.sql with updated files for tables with books
    replaced database.sql and dummy.sql with updated files for tables with books. No images uploaded yet, and have not gotten any of the descriptiveve text to show, but the correct amount of inventory items now shows. Included screenshot in lib folder with the updated core.php

--- html/sql/database-originalWorking.sql
+++ html/sql/database-originalWorking.sql
@@ -18,29 +18,25 @@ ALTER TABLE `orders`
 -- (B) ORDERS ITEMS TABLE --
 CREATE TABLE `orders_items` (
   `order_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `orders_items`
-  ADD PRIMARY KEY (`order_id`,`book_id`);
+  ADD PRIMARY KEY (`order_id`,`product_id`);
 
 -- (C) PRODUCTS TABLE --
 CREATE TABLE `products` (
-  `book_id` int(11) NOT NULL,
-  `book_title` varchar(255) NOT NULL,
-  `book_author` varchar(255) NOT NULL,
-  `pages` int(11) NOT NULL,
-  `book_Format` varchar(255) NOT NULL,
-  `book_coverImg` varchar(255) DEFAULT NULL,
-  `book_description` text DEFAULT NULL,
-  `book_price` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `publisher` varchar(255) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` varchar(255) DEFAULT NULL,
+  `product_description` text DEFAULT NULL,
+  `product_price` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`book_id`),
-  ADD KEY `name` (`book_title`);
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `name` (`product_name`);
 
 ALTER TABLE `products`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
